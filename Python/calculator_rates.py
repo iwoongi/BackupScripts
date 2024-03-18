46967bd8823612de5bf9c2e1a3f0c6c4166c36c9
woong <50078116+iwoongi@users.noreply.github.com>
2024-03-18 16:42:36 +0900

    aa

--- Python/calculator_rates.py
+++ Python/calculator_rates.py
@@ -3,13 +3,13 @@ import math
 from dateutil.relativedelta import relativedelta
 
 
-
 startDay = datetime.datetime.now()
 finalDay = None
 
 ratePercent = 2
 input_money = 50
 cycle_month = 12
+
 
 def calculator_addDayRate():
     global finalDay
@@ -21,7 +21,7 @@ def calculator_addDayRate():
 
     dayRate = calculator_dayRate()
     print(f"{dayRate:.8f} : 하루이자 {input_money*dayRate:.3f}")
-    
+
     final_money = 0
     for a in range(0, cycle_month, 1):
         final_money = input_money + money_oneCycle(a, final_money)
@@ -30,19 +30,22 @@ def calculator_addDayRate():
     # for a in range(0, cycle_month, 1):
     #     pass
 
+
 # 하루기준금리
 def calculator_dayRate():
     rate = ratePercent / 100
     return rate / 365
 
+
 # 입금단위
 def money_manwon():
     return input_money * 10000
 
+
 def money_oneCycle(index, money):
     newStartDay = startDay + relativedelta(months=index)
     days = (finalDay - newStartDay).days
-    if index==0:
+    if index == 0:
         money = input_money
 
     final_money = money * ((1 + calculator_dayRate()) ** days)
